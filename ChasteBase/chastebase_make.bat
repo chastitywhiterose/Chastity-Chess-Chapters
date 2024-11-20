df3d5b13c9d22d10bd509c593bc0e90d00145e9e
Chastity White Rose <chastitywhiterose@gmail.com>
2024-11-20 15:51:42 -0600

    Add files via upload

--- ChasteBase/chastebase_make.bat
+++ ChasteBase/chastebase_make.bat
@@ -5,3 +5,5 @@ pgn-extract -TwChastityWhiteRose -Tr1-0 chastitywhiterose_chesscom.pgn -ochasteb
 pgn-extract -TbChastityWhiteRose -Tr0-1 chastitywhiterose_chesscom.pgn -ochastebase_black_wins_chesscom.pgn
 
 pgn-extract chastebase_white_wins_lichess.pgn chastebase_black_wins_lichess.pgn chastebase_white_wins_chesscom.pgn chastebase_black_wins_chesscom.pgn -ochastebase_all_wins.pgn
+
+pgn-extract chastebase_all_wins.pgn --checkmate -ochastebase_all_wins_checkmate.pgn
