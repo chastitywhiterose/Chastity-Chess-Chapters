e559cd85a7a7bbc89e4e41913e0bbdd35a0a66cb
Chastity White Rose <chastitywhiterose@gmail.com>
2024-12-12 18:27:25 -0600

    Add files via upload

--- ChasteBase/chastebase_make.bat
+++ ChasteBase/chastebase_make.bat
@@ -1,12 +1,15 @@
 rem pgn-extract: Make a list of ALL my wins on lichess.org and chess.com that end in a checkmate!
-rem The original pgn files are created by downloading my full database of games from lichess.org and chess.com using En-Croissant
+rem The original pgn files are created by downloading my full databases of games from lichess.org and chess.com using En-Croissant
+rem This script has been customized specifically to use both platforms because I am unusual in that I play on both.
 
-pgn-extract -Twchastitywhiterose -Tr1-0 chastitywhiterose_lichess.pgn -ochastebase_white_wins_lichess.pgn --checkmate
-pgn-extract -Tbchastitywhiterose -Tr0-1 chastitywhiterose_lichess.pgn -ochastebase_black_wins_lichess.pgn --checkmate
+set user=chastity
 
-pgn-extract -TwChastityWhiteRose -Tr1-0 chastitywhiterose_chesscom.pgn -ochastebase_white_wins_chesscom.pgn --checkmate
-pgn-extract -TbChastityWhiteRose -Tr0-1 chastitywhiterose_chesscom.pgn -ochastebase_black_wins_chesscom.pgn --checkmate
+pgn-extract -Twchastitywhiterose -Tr1-0 chastitywhiterose_lichess.pgn -o%user%_white_wins_lichess.pgn --checkmate
+pgn-extract -Tbchastitywhiterose -Tr0-1 chastitywhiterose_lichess.pgn -o%user%_black_wins_lichess.pgn --checkmate
 
-pgn-extract chastebase_white_wins_lichess.pgn chastebase_white_wins_chesscom.pgn -ochastebase_all_wins_white.pgn
-pgn-extract chastebase_black_wins_lichess.pgn chastebase_black_wins_chesscom.pgn -ochastebase_all_wins_black.pgn
+pgn-extract -TwChastityWhiteRose -Tr1-0 chastitywhiterose_chesscom.pgn -o%user%_white_wins_chesscom.pgn --checkmate
+pgn-extract -TbChastityWhiteRose -Tr0-1 chastitywhiterose_chesscom.pgn -o%user%_black_wins_chesscom.pgn --checkmate
+
+pgn-extract %user%_white_wins_lichess.pgn %user%_white_wins_chesscom.pgn -o%user%_all_wins_white.pgn
+pgn-extract %user%_black_wins_lichess.pgn %user%_black_wins_chesscom.pgn -o%user%_all_wins_black.pgn
 
