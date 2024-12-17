213f1f3e70cc45510521bd3afd28bed1fa6c0723
Chastity White Rose <chastitywhiterose@gmail.com>
2024-12-17 05:13:31 -0600

    Add files via upload

--- ChasteBase/chesscom_top3_make.bat
+++ ChasteBase/chesscom_top3_make.bat
@@ -19,7 +19,7 @@ pgn-extract -Tw%master% -Tr1-0 -xwhite_openings.txt %database% -o%master%_white_
 pgn-extract -Tb%master% -Tr0-1 -xblack_openings.txt %database% -o%master%_black_wins_chesscom.pgn --checkmate
 
 
-pgn-extract Hikaru_white_wins_chesscom.pgn MagnusCarlsen_white_wins_chesscom.pgn Firouzja2003_white_wins_chesscom.pgn -otop3_master_white_wins
+pgn-extract Hikaru_white_wins_chesscom.pgn MagnusCarlsen_white_wins_chesscom.pgn Firouzja2003_white_wins_chesscom.pgn -otop3_master_white_wins.pgn
 
-pgn-extract Hikaru_black_wins_chesscom.pgn MagnusCarlsen_black_wins_chesscom.pgn Firouzja2003_black_wins_chesscom.pgn -otop3_master_black_wins
+pgn-extract Hikaru_black_wins_chesscom.pgn MagnusCarlsen_black_wins_chesscom.pgn Firouzja2003_black_wins_chesscom.pgn -otop3_master_black_wins.pgn
 
