2106e71740f1fe8cdc97f95427ff7c26dc4f55d0
Chastity White Rose <chastitywhiterose@gmail.com>
2024-12-17 04:58:53 -0600

    Add files via upload

---
+++ ChasteBase/chesscom_top3_make.bat
@@ -0,0 +1,25 @@
+rem pgn-extract: Make database of top 3 Chess.com players. Only checkmate endings with specific openings I want.
+
+set database=Hikaru_chesscom.pgn
+set master=Hikaru
+
+pgn-extract -Tw%master% -Tr1-0 -xwhite_openings.txt %database% -o%master%_white_wins_chesscom.pgn --checkmate
+pgn-extract -Tb%master% -Tr0-1 -xblack_openings.txt %database% -o%master%_black_wins_chesscom.pgn --checkmate
+
+set database=MagnusCarlsen_chesscom.pgn
+set master=MagnusCarlsen
+
+pgn-extract -Tw%master% -Tr1-0 -xwhite_openings.txt %database% -o%master%_white_wins_chesscom.pgn --checkmate
+pgn-extract -Tb%master% -Tr0-1 -xblack_openings.txt %database% -o%master%_black_wins_chesscom.pgn --checkmate
+
+set database=Firouzja2003_chesscom.pgn
+set master=Firouzja2003
+
+pgn-extract -Tw%master% -Tr1-0 -xwhite_openings.txt %database% -o%master%_white_wins_chesscom.pgn --checkmate
+pgn-extract -Tb%master% -Tr0-1 -xblack_openings.txt %database% -o%master%_black_wins_chesscom.pgn --checkmate
+
+
+pgn-extract Hikaru_white_wins_chesscom.pgn MagnusCarlsen_white_wins_chesscom.pgn Firouzja2003_white_wins_chesscom.pgn -otop3_master_white_wins
+
+pgn-extract Hikaru_black_wins_chesscom.pgn MagnusCarlsen_black_wins_chesscom.pgn Firouzja2003_black_wins_chesscom.pgn -otop3_master_black_wins
+
